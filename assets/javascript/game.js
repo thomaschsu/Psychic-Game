c7984940b1a651785bcdbc0eea4203025aa781c0
Tom Hsu <thomaschsu@gmail.com>
2018-05-15 00:59:54 -0400

    Modified game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
-// If letter right, mark as a win and restart
 // If player exhausts all guesses, mark as a loss and restart
-
-var wins = 0;
-var losses = 0;
-var guesses = 9;
 var letters =
 ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 // Empty array for guessed letters
 var guessedLetters = [];
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
 
 // Computer chooses random letter
 var compRand = letters[Math.floor(Math.random() * letters.length)];
@@ -20,20 +19,29 @@ document.onkeyup = function (event) {
     console.log(playRand);
 
 // If letter is wrong, mark down letter as previously guessed
+    if ((playRand === 'a') || (playRand === 'b') || (playRand === 'c') || (playRand === 'd') || (playRand === 'e') || (playRand === 'f') || (playRand === 'g') || (playRand === 'h') || (playRand === 'i') || (playRand === 'j') || (playRand === 'k') || (playRand === 'l') || (playRand === 'm') || (playRand === 'n') || (playRand === 'o') || (playRand === 'p') || (playRand === 'q') || (playRand === 'r') || (playRand === 's') || (playRand === 't') || (playRand === 'u') || (playRand === 'v') || (playRand === 'w') || (playRand === 'x') || (playRand === 'y') || (playRand === 'z')) {
+
     if (compRand != playRand) {
         (guesses-- && guessedLetters.push(playRand));
         console.log(guesses);
     }
 
 // If letter right, mark as a win and restart
-    if (compRand === playRand) {
+    else if (compRand == playRand) {
         wins++;
         console.log(wins);
     }
+
+    else if (guesses == 0) {
+        losses++;
+    }
+}
+
     var html =
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Losses: " + losses + "</p>" +
-        "<p>Guesses left: " + guesses + "</p>" +
-        "<p>Your guesses so far: " + guessedLetters + "</p>";
-        document.querySelector("#game").innerHTML = html;
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses left: " + guesses + "</p>" +
+    "<p>Your guesses so far: " + guessedLetters + "</p>";
+    document.querySelector("#game").innerHTML = html;
+
 };
