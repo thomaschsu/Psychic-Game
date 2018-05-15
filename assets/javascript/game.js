69dca0a82a63f915efb1c027a74699af0545b461
Tom Hsu <thomaschsu@gmail.com>
2018-05-15 13:59:52 -0400

    Modified js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,13 +13,18 @@ var compRand = letters[Math.floor(Math.random() * letters.length)];
 console.log(compRand);
 
 // Player chooses random letter
-document.onkeyup = function (event) {
+window.onload = document.onkeyup = function (event) {
     var wins = 0;
     var playRand = event.key;
     console.log(playRand);
 
 // If letter is wrong, mark down letter as previously guessed
-    if ((playRand === 'a') || (playRand === 'b') || (playRand === 'c') || (playRand === 'd') || (playRand === 'e') || (playRand === 'f') || (playRand === 'g') || (playRand === 'h') || (playRand === 'i') || (playRand === 'j') || (playRand === 'k') || (playRand === 'l') || (playRand === 'm') || (playRand === 'n') || (playRand === 'o') || (playRand === 'p') || (playRand === 'q') || (playRand === 'r') || (playRand === 's') || (playRand === 't') || (playRand === 'u') || (playRand === 'v') || (playRand === 'w') || (playRand === 'x') || (playRand === 'y') || (playRand === 'z')) {
+    if ((playRand === 'a') || (playRand === 'b') || (playRand === 'c') || (playRand === 'd') || (playRand === 'e')
+    || (playRand === 'f') || (playRand === 'g') || (playRand === 'h') || (playRand === 'i') || (playRand === 'j') ||
+    (playRand === 'k') || (playRand === 'l') || (playRand === 'm') || (playRand === 'n') || (playRand === 'o') ||
+    (playRand === 'p') || (playRand === 'q') || (playRand === 'r') || (playRand === 's') || (playRand === 't') ||
+    (playRand === 'u') || (playRand === 'v') || (playRand === 'w') || (playRand === 'x') || (playRand === 'y') ||
+    (playRand === 'z')) {
 
     if (compRand != playRand) {
         (guesses-- && guessedLetters.push(playRand));
@@ -27,12 +32,13 @@ document.onkeyup = function (event) {
     }
 
 // If letter right, mark as a win and restart
-    else if (compRand == playRand) {
+    if (compRand == playRand) {
         wins++;
         console.log(wins);
+        location.reload();
     }
 
-    else if (guesses == 0) {
+    if (guesses == 0) {
         losses++;
         console.log(losses);
     }
@@ -42,7 +48,7 @@ document.onkeyup = function (event) {
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" +
     "<p>Guesses left: " + guesses + "</p>" +
-    "<p>Your guesses so far: " + guessedLetters + "</p>";
+    "<p>Your Guesses so far: " + guessedLetters + " " + "</p>";
     document.querySelector("#game").innerHTML = html;
 
 };
