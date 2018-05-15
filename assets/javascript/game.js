a1f6a5eccba17f916f8d8d7ac08de2872fbb9719
Tom Hsu <thomaschsu@gmail.com>
2018-05-15 00:05:33 -0400

    Modified game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Computer chooses random letter
+// Player chooses random letter
+// If letter wrong, mark down letter as previously guessed
+// If letter right, mark as a win and restart
+// If player exhausts all guesses, mark as a loss and restart
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+
